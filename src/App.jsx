7cbf29363a5443c905f93d3cf282d01805b1f77e
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-22 19:27:51 +0400

    add Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Counter from "./components/Counter";
 // shift+alt+o -> remove unused imports
 // dfgfnhg
 function App() {
     return (
         <>
             {/* <Ali /> */}
-            <Header />
+            {/* <Header /> */}
+            <Counter />
         </>
     )
 }
